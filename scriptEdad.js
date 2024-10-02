35d9743a6a356d5bbc357dd8fdd7507257dcdcad
jorge-arma <73048447+jorge-arma@users.noreply.github.com>
2024-10-01 21:22:23 -0500

    ejercicios de edad

---
+++ scriptEdad.js
@@ -0,0 +1,29 @@
+function verificarEdad() {
+
+   console.log("Ingreso a verificarEdad");
+   let edad = document.getElementById('edad').value;
+   let resultado = document.getElementById('resultado');
+
+   //Eliminar clases previamente aplicadas
+   resultado.classList.remove('mensaje-error', 'mensaje-invalido', 'mensaje-valido');
+
+
+   if (edad == "") {
+       resultado.textContent = " Por favor, ingresa tu edad";
+       resultado.classList.add('mensaje-error');
+   } else if (edad <= 0) {
+       resultado.textContent = "La edad no puede ser negativa o igual a 0";
+       resultado.classList.add('mensaje-invalido');
+   } else if (edad.length >= 3) {
+       resultado.textContent = "La longitud de la edad es mayor de la esperada";
+       resultado.classList.add('mensaje-invalido');
+   } else if (edad >= 18) {
+       resultado.textContent = "Eres una persona mayor de edad puedes ingresar al sitio";
+       resultado.classList.add('mensaje-valido');
+   } else {
+       resultado.textContent = "Es menor de edad, no puedes ingresar al sitio ";
+       resultado.classList.add('mensaje-error');
+   }
+
+}
+
